8c40387da26ca377a559dfb564eaf5561a595188
SolveNaN <dagoberto6993@gmail.com>
2023-02-12 19:09:20 -0500

    Version de despliegue, delete no funciona

---
+++ MarlonControWell/Client/Program.cs
@@ -0,0 +1,14 @@
+global using MarlonControWell.Client.Services.PozoService;
+using MarlonControWell.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CurrieTechnologies.Razor.SweetAlert2;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<IPozoService, PozoService>();
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSweetAlert2();
+await builder.Build().RunAsync();   
